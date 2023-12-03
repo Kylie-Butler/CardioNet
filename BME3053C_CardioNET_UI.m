7c10c763984526900754fda611aa1c360890f7fa
madmac004 <151090529+madmac004@users.noreply.github.com>
2023-12-02 21:44:41 -0500

    Add files via upload

---
+++ BME3053C_CardioNET_UI.m
@@ -0,0 +1,67 @@
+%% CardioNet
+% Author: Madeleine McCreary
+% Group Members: Kylie Butler, Andrew Dodds
+% Course: BME 3053C Computer Applications for BME
+% Term: Fall 2023
+% J. Crayton Pruitt Family Department of Biomedical Engineering
+% University of Florida
+% Email: mmccreary@ufl.edu
+% December 4, 2023
+
+fprintf(['Welcome to CardioNet! Begin entering patient info below \n']);
+
+%% Ask for user inputs
+age = input('Age: ');
+sex = input('Sex (M or F): ', 's');
+if strcmp(sex,'M') == 1
+    sex = 1;
+else
+    sex = 0;
+end
+chest_Pain = input('Chest Pain Type (0-3): ');
+rest_BP = input('Resting Blood Pressure [mmHg]: ');
+cholesterol = input('Serum Cholesterol [mg/dl]: ');
+fasting_BS = input('Fasting Blood Sugar > 120 mg/dl (Y or N): ');
+if strcmp(fasting_BS,'Y') == 1
+    fasting_BS = 1;
+else
+    fasting_BS = 0;
+end
+max_HR = input('Maximum Heartrate: ');
+exInd_Angina = input('Exercise Induced Angina (Y or N): ', 's');
+if strcmp(exInd_Angina,'Y') == 1
+    exInd_Angina = 1;
+else
+    exInd_Angina = 0;
+end
+thal = input('Thalamus Stress results (0-7): ');
+
+Heart_Data = readtable("Cleveland_Heart_Data.xlsx");
+Age = [table2array(Heart_Data(:,1)); age];
+Sex = [table2array(Heart_Data(:,2)); sex];
+Chest_Pain = [table2array(Heart_Data(:,3)); chest_Pain];
+Rest_BP = [table2array(Heart_Data(:,4)); rest_BP];
+Cholesterol = [table2array(Heart_Data(:,6)); cholesterol]; 
+Fasting_BS = [table2array(Heart_Data(:,5)); fasting_BS];
+Max_HR = [table2array(Heart_Data(:,8)); max_HR];
+ExInd_Angina = [table2array(Heart_Data(:,9)); exInd_Angina];
+Thal = [table2array(Heart_Data(:,13)); thal];
+
+    
+%% Normalize features
+Features1 = [Age, Sex, Chest_Pain, Rest_BP, Cholesterol, Fasting_BS, Max_HR, ExInd_Angina, Thal];
+Features1 = mapminmax(Features1)';
+
+%% Load pre-trained Neural Network and run new data
+load('CardioNet_90.mat');
+
+output = CardioNet(Features1(1:9,298));
+
+%% Interpretation of neural network results
+if output<0.3
+    fprintf('The patient is not at risk of Coronary Artery Disease \n')
+elseif output>=0.3&&output<=0.7
+    fprintf('The patient has a moderate risk of Coronary Artery Disease. Further testing required. \n')
+else 
+    fprintf('The patient is at high risk of Coronary Artery Disease. Please refer to cardiologist. \n')
+end
--- BME3053C_Final.m
+++ BME3053C_Final.m
@@ -1,46 +1,71 @@
-%% Retrying 
+%% Training the CardioNet Neural Network
+% Author: Madeleine McCreary
+% Group Members: Kylie Butler, Andrew Dodds
+% Course: BME 3053C Computer Applications for BME
+% Term: Fall 2023
+% J. Crayton Pruitt Family Department of Biomedical Engineering
+% University of Florida
+% Email: mmccreary@ufl.edu
+% December 4, 2023
+
+%% Import dataset
 Heart_Data = readtable("Cleveland_Heart_Data.xlsx");
 Age = table2array(Heart_Data(:,1));
-    Sex = table2array(Heart_Data(:,2));
-    Chest_Pain = table2array(Heart_Data(:,3));
-    Rest_BP = table2array(Heart_Data(:,4));
-    Cholesterol = table2array(Heart_Data(:,6)); 
-    Fasting_BS = table2array(Heart_Data(:,5));
-    Max_HR = table2array(Heart_Data(:,8));
-    ExInd_Agnia = table2array(Heart_Data(:,9));
-    Thal = table2array(Heart_Data(:,13));
-    Disease = table2array(Heart_Data(:,14));
+Sex = table2array(Heart_Data(:,2));
+Chest_Pain = table2array(Heart_Data(:,3));
+Rest_BP = table2array(Heart_Data(:,4));
+Cholesterol = table2array(Heart_Data(:,6)); 
+Fasting_BS = table2array(Heart_Data(:,5));
+Max_HR = table2array(Heart_Data(:,8));
+ExInd_Agnia = table2array(Heart_Data(:,9));
+Thal = table2array(Heart_Data(:,13));
+Disease = table2array(Heart_Data(:,14));
     
-    %Normalize features
-    features = [Age, Sex, Chest_Pain, Rest_BP, Cholesterol, Fasting_BS, Max_HR, ExInd_Agnia, Thal];
-    features = mapminmax(features)';
+%% Normalize features
+features = [Age, Sex, Chest_Pain, Rest_BP, Cholesterol, Fasting_BS, Max_HR, ExInd_Agnia, Thal];
+features = mapminmax(features)';
     
-    %% Neural Network Training
-    CardioNet = feedforwardnet([10 5]);
-    CardioNet.trainFcn = 'trainlm'; % Levenberg-Marquardt backpropagation
-    CardioNet.trainParam.showWindow = true;
-    targets = Disease(1:275)';
-    [CardioNet, tr] = train(CardioNet, features(:,1:275), targets);
-    
-    %% Specify Division of Data
-    CardioNet.divideParam.trainRatio = 0.7;
-    CardioNet.divideParam.valRatio = 0.15;
-    CardioNet.divideParam.testRatio = 0.15;
+%% Neural Network Training
+CardioNet = feedforwardnet([10 5]);
+CardioNet.trainFcn = 'trainlm'; % Levenberg-Marquardt backpropagation
+CardioNet.trainParam.showWindow = true;
+targets = Disease(1:267)';
+[CardioNet, tr] = train(CardioNet, features(:,1:267), targets);
+
+%% Specify Division of Data
+CardioNet.divideParam.trainRatio = 0.7;
+CardioNet.divideParam.valRatio = 0.15;
+CardioNet.divideParam.testRatio = 0.15;
     
 %% Test
 count = 0;
-for ii = 276:297
+true_pos = 0;
+true_neg = 0;
+false_neg = 0;
+false_pos = 0;
+
+for ii = 268:297
     output = CardioNet(features(:,ii))
     real = Disease(ii)
-    if (output<0.5 && real==0) || (output>=0.05 && real==1)
+    if output<0.5 && real==0
+        true_neg = true_neg + 1;
         fprintf('Successful Prediction')
         count = count + 1;
-    else
+    elseif output>=0.5 && real==1
+        true_pos = true_pos + 1;
+        fprintf('Successful Prediction')
+        count = count + 1;
+    elseif output>=0.5 && real==0;
         fprintf('Incorrect Prediction')
+        false_pos = false_pos + 1;
+    elseif output<0.5 && real==1
+        fprintf('Incorrect Prediction')
+        false_neg = false_neg + 1;
     end
 end
-succcess_rate = count/(297-275);
-save CardioNet
+succcess_rate = count/(297-267);
+filename = 'CardioNet.mat';
+save(filename, 'CardioNet');
 %     %% View the trained network
 %     view(net);
 %     
